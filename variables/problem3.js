1cf00196392d10ea539466082e1a64b2f419e076
keertiPawar8 <keertipawar8@gmail.com>
2022-09-04 20:02:43 +0530

    Variables problems

---
+++ variables/problem3.js
@@ -0,0 +1,8 @@
+// Problem 3
+// Create a variable name to store your name
+// Create a variable age to store your age
+// Print the values stored in the variable on one line followed by the type of the variable in the next line
+var name = "Keerti Pawar";
+  var age = 21;
+console.log(name,age)
+console.log( typeof name, typeof age)
---
+++ variables/problem4.js
@@ -0,0 +1,21 @@
+// Problem 4
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols )
+
+var name = "Name - Keerti Pawar"
+var school = "School -  St. Francis school,sarni"
+var grade = "grade - 12th "
+var section = "section - A"
+var rollno = "rollno - 2122"
+var maths = "marks of maths is 78 "
+var science = "marks of science is 89 "
+var hindi = "marks of hindi is 78"
+console.log("REPORT CARD")
+console.log(name)
+console.log(school)
+console.log(grade)
+console.log(section)
+console.log(rollno)
+console.log(maths)
+console.log(science)
+console.log(hindi)
