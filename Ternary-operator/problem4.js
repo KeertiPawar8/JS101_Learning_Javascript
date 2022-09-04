d331a2a2682e51d31b50508f76641b912f48f094
keertiPawar8 <keertipawar8@gmail.com>
2022-09-04 21:15:17 +0530

    Ternaryoperatorproblems

---
+++ Ternary-operator/problem4.js
@@ -0,0 +1,8 @@
+// Problem 4: Given 3 numbers (all different values), print which is greatest
+let a = 85;
+let b = 67;
+let c = 58;
+
+a > b && a > c ? console.log("a is greatest")
+  : b > a && b > c ? console.log("b is greatest")
+    : console.log("c is greatest")
---
+++ Ternary-operator/problem5.js
@@ -0,0 +1,19 @@
+// Problem 5: Given the days of the week in short format "Sun", "Mon" ... print in long format "Sunday", "Monday", ...
+let day = "SUN";
+switch (day) {
+  case "MON": console.log("Monday");
+    break;
+  case "TUE": console.log("tuesday");
+    break;
+  case "WED": console.log("wednesday");
+    break;
+  case "THU": console.log("thursday");
+    break;
+  case "FRI": console.log("friday");
+    break;
+  case "SAT": console.log("saturday");
+    break;
+  case "SUN": console.log("sunday");
+    break;
+  default: console.log("Wrong Input");
+}
